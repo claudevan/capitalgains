d18a7b8987619221ffb96bbd1e4d7fea976b870d
Claudevan <claudevan.ti@gmail.com>
2025-05-08 10:46:51 -0300

    init: first commit

--- capitalgains/src/main/java/github/com/claudevan/capitalgains/CapitalGainsCommands.java
+++ capitalgains/src/main/java/github/com/claudevan/capitalgains/CapitalGainsCommands.java
@@ -39,28 +39,27 @@ public class CapitalGainsCommands {
 
         for (Operation op : operations) {
             if (op.getOperation().equals("buy")) {
+                // Recalcula o preço médio ponderado
                 BigDecimal totalCost = weightedAverageCost.multiply(BigDecimal.valueOf(totalQuantity))
                         .add(op.getUnitCost().multiply(BigDecimal.valueOf(op.getQuantity())));
                 totalQuantity += op.getQuantity();
                 weightedAverageCost = totalCost.divide(BigDecimal.valueOf(totalQuantity), 2, RoundingMode.HALF_UP);
                 results.add(new TaxResult(BigDecimal.ZERO));
-
             } else if (op.getOperation().equals("sell")) {
+                // Calcula o valor total da operação de venda
                 BigDecimal totalSellValue = op.getUnitCost().multiply(BigDecimal.valueOf(op.getQuantity()));
 
-                if (totalSellValue.compareTo(BigDecimal.valueOf(20000)) <= 0) {
-                    results.add(new TaxResult(BigDecimal.ZERO));
-                    continue;
-                }
-
+                // Calcula o custo total baseado no preço médio ponderado
                 BigDecimal totalCost = weightedAverageCost.multiply(BigDecimal.valueOf(op.getQuantity()));
-                BigDecimal profit = totalSellValue.subtract(totalCost);
+                BigDecimal profit = totalSellValue.subtract(totalCost); // Lucro ou prejuízo da operação
 
                 if (profit.compareTo(BigDecimal.ZERO) > 0) {
+                    // Caso de lucro: deduz o prejuízo acumulado antes de calcular o imposto
                     if (accumulatedLoss.compareTo(BigDecimal.ZERO) > 0) {
                         BigDecimal adjustedProfit = profit.subtract(accumulatedLoss);
+
                         if (adjustedProfit.compareTo(BigDecimal.ZERO) > 0) {
-                            accumulatedLoss = BigDecimal.ZERO;
+                            accumulatedLoss = BigDecimal.ZERO; // Prejuízo completamente deduzido
                             BigDecimal tax = adjustedProfit.multiply(BigDecimal.valueOf(0.2));
                             results.add(new TaxResult(tax));
                         } else {
@@ -68,14 +67,24 @@ public class CapitalGainsCommands {
                             results.add(new TaxResult(BigDecimal.ZERO));
                         }
                     } else {
+                        // Sem prejuízo acumulado, calcula imposto normalmente
+
+                        // Regras para isenção de imposto (valor total <= R$ 20.000,00)
+                        if (totalSellValue.compareTo(BigDecimal.valueOf(20000)) <= 0) {
+                            results.add(new TaxResult(BigDecimal.ZERO));
+                           continue;
+                        }
+
                         BigDecimal tax = profit.multiply(BigDecimal.valueOf(0.2));
                         results.add(new TaxResult(tax));
                     }
                 } else {
+                    // Caso de prejuízo: acumula o prejuízo
                     accumulatedLoss = accumulatedLoss.add(profit.abs());
                     results.add(new TaxResult(BigDecimal.ZERO));
                 }
 
+                // Atualiza a quantidade total de ações
                 totalQuantity -= op.getQuantity();
             }
         }
